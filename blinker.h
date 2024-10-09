f469594e41a6d2eda7f5381aa76174dc7b050431
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-10-09 10:02:17 +0300

    Update blinker.h

--- blinker.h
+++ blinker.h
@@ -1,2 +1,17 @@
 #include <Arduino.h>
 #include <Wire.h>
+
+int blinker(int pin) {
+  if (!pin) return 1;
+  if (!Wire.hasPin(pin)) return 2;
+}
+
+int blinker(int pin, int num) {
+  if (!pin) return 1;
+  if (!Wire.hasPin(num)) return 2;
+}
+
+int blinker(int pin, int num, int len) {
+  if (!pin) return 1;
+  if (!Wire.hasPin(num)) return 2;
+}
