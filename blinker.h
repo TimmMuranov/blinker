129784165b0ddfc039fe32879c358eb4c799c838
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-10-09 10:46:56 +0300

    Update blinker.h

--- blinker.h
+++ blinker.h
@@ -1,48 +1,42 @@
 #include <Arduino.h>
 #include <Wire.h>
-
-int blinker(int pin) {
-  if (!pin) return 1;
-  if (!Wire.hasPin(pin)) return 2;
+//=============================
+void blinker(int pin) {
+  if (!Wire.hasPin(pin)) return;
   digitalWrite(pin, HIGH);
   delay(500);
   digitalWrite(pin, LOW);
 }
-
-int blinker(int pin, int num) {
-  if (!pin) return 1;
-  if (!Wire.hasPin(num)) return 2;
-  for (int x=0; x<num; ++x){
+//=============================
+void blinker(int pin, int num) {
+  if (!Wire.hasPin(num)) return;
+  for (int x = 0; x < num; ++x) {
     digitalWrite(pin, HIGH);
     delay(500);
     digitalWrite(pin, LOW);
-    delay(500;
+    delay(500);
   }
 }
-
-int blinker(int pin, int num, int len1, int len2) {
-  if (!pin) return 1;
-  if (!Wire.hasPin(num)) return 2;
-  for (int x=0; x<num; ++x){
+//=============================
+void blinker(int pin, int num, int len1, int len2) {
+  if (!Wire.hasPin(num)) return;
+  for (int x = 0; x < num; ++x) {
     digitalWrite(pin, HIGH);
     delay(len1);
     digitalWrite(pin, LOW);
     delay(len2);
   }
 }
-
-int blinker(int pin, int num, int len1, int len2, bool rev) {
-  if (!pin) return 1;
-  if (!Wire.hasPin(num)) return 2;
-  string h = "HIGH";
-  string l = "LOW";
-  
-  if (rev){
+//=============================
+void blinker(int pin, int num, int len1, int len2, bool rev) {
+  if (!Wire.hasPin(num)) return;
+  const char *h = "HIGH";
+  const char *l = "LOW";
+  if (rev) {
     h = "LOW";
     l = "HIGH";
   }
-  
-  for (int x=0; x<num; ++x){
+  for (int x = 0; x < num; ++x) {
     digitalWrite(pin, h);
     delay(len1);
     digitalWrite(pin, l);
